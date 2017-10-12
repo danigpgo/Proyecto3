6b720f1f5b71e74fce0c63d9f7f960f472fbd9fe
Danigp <danielentj@gmail.com>
2017-10-12 19:01:12 +0200

    arreglo heroku

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
+// export const environment = {
+//   production: true,
+//   BASEURL: ''
+// };
+
+
 export const environment = {
-  production: true,
-  BASEURL: ''
+  production: false,
+  BASEURL: 'http://localhost:3000'
 };
--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-// const favicon = require('serve-favicon');
+const favicon = require('serve-favicon');
 const session    = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const logger = require('morgan');
@@ -36,8 +36,8 @@ app.use(cors(corsOptions));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
