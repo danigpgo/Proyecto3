365fd078300d77d1fcc6b3a270556e69f1bf365b
Danigp <danielentj@gmail.com>
2017-10-13 11:06:53 +0200

    arreglando base de datos mlab

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,10 @@
 const path = require('path');
 const debug = require('debug')("angularauth:"+path.basename(__filename).split('.')[0]);
 const mongoose = require('mongoose');
-const dbURL = process.env.DBURL || 'mongodb://localhost/angular-auth';
+const dbURL = process.env.DBURL
+
+// || 'mongodb://localhost/angular-auth';
+
 
 mongoose.connect(dbURL)
   .then(() => debug(`connected to database ${dbURL}`))
